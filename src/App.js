579b707b9dbafc414924bae52eafa01db759e245
Aayush <jaayush014@gmail.com>
2022-07-05 21:08:11 +0000

    feat: create new resume

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
   const [resumeData, setResumeData] = useState({});
 
   useEffect(() => {
-    fetch("/resumeData.json")
+    fetch("/newResume.json")
       .then((res) => res.json())
       .then((data) => {
         setResumeData(data);
